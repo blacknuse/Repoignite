04e41c86882dc5087b43179ba60d1a382dcdd47e
blacknuse <exciamatlonnone@gmail.com>
2026-09-01 16:27:57 +0900

    Use Rust Windows subsystem attribute instead of linker override

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,1 @@
-fn main() {
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-arg-bin=osirhidden=/SUBSYSTEM:WINDOWS");
-    tauri_build::build();
-}
+fn main() { tauri_build::build() }
