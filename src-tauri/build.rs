e5aa80554fbf8df7a7e4872874c77b7eb1117e32
blacknuse <exciamatlonnone@gmail.com>
2026-09-01 16:20:04 +0900

    Build Windows release as GUI subsystem

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,1 +1,5 @@
-fn main() { tauri_build::build() }
+fn main() {
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-arg-bin=osirhidden=/SUBSYSTEM:WINDOWS");
+    tauri_build::build();
+}
